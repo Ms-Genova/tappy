8f32eece8f2df307c7be3a5ea03b56c3082a0753
Ms-Genova <genovae@sacredheartcollege.school.nz>
2024-10-31 10:09:05 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . b 3 3 b . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 3 3 3 3 3 b . . . 
+    . b b b b b 3 3 3 3 3 3 3 b . . 
+    . b d 3 b 3 3 3 3 3 3 3 3 b . . 
+    . . b 3 3 b 3 d 3 f 3 d 4 f . . 
+    . . b d 3 3 b 1 f f 3 4 4 c . . 
+    b b d b 3 3 3 d f b 4 4 4 4 b . 
+    b d d c d 3 3 b 3 4 4 4 4 4 4 b 
+    c d d d c c b 3 3 3 3 3 3 3 b . 
+    c b d d d d d 3 3 3 3 3 3 3 b . 
+    . c d d d d d d 3 3 3 3 3 d b . 
+    . . c b d d d d d 3 3 3 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
